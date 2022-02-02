f41be56249cd4412f64160b525271bf31dc1b7f4
elesian <genovacle@hotmail.com>
2022-02-02 13:22:43 +0000

    Thumbnail buffering issue resolved

--- src/VideoList.js
+++ src/VideoList.js
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import publicAPIKey from './publicAPIKey';
 import { useEffect } from 'react';
 import VideoPlayer from './VideoPlayer';
+let Buffer = require('buffer/').Buffer;
+
+const axios = require('axios').default;
 
 const VideoList = ({ search }) => {
   const searchTerm = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&publishedBefore=2008-01-01T00%3A00%3A00Z&q=${search}&key=${publicAPIKey}`;
@@ -11,13 +14,31 @@ const VideoList = ({ search }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [videoId, setVideoId] = useState('No video selected');
 
+  const getBase64 = (url) => {
+    console.log(url);
+    return axios
+      .get(`https://murmuring-journey-05155.herokuapp.com/${url}`, {
+        responseType: 'arraybuffer',
+      })
+      .then((response) => {
+        const image = Buffer.from(response.data, 'binary').toString('base64');
+        return image;
+      })
+      .catch((err) => console.log('ERROR' + err));
+  };
+
   useEffect(() => {
     if (search !== '') {
       fetch(searchTerm)
         .then((res) => res.json())
-        .then((videosFromApi) => {
-          console.log(videosFromApi.items);
-          setList(videosFromApi.items);
+        .then(({ items }) => {
+          items.map((item) => {
+            getBase64(item.snippet.thumbnails.default.url).then((image) => {
+              return (items.thumbnailUrl = image);
+            });
+          });
+          console.log(items);
+          setList(items);
           setIsLoading(false);
         });
     }
@@ -31,7 +52,7 @@ const VideoList = ({ search }) => {
   else
     return (
       <div className="task-list">
-        <h2>Not Loading</h2>
+        <p>Loaded</p>
         {list.length !== 0 ? (
           <ul className="search-results">
             {list.map((item) => {
